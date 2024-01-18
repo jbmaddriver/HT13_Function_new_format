721f518175dce8ee47de179d95ebfa2cbb5584b7
jbmaddriver <143184435+jbmaddriver@users.noreply.github.com>
2024-01-18 13:18:27 +0200

    HT33 Async code. Updates2.

--- async.py
+++ async.py
@@ -39,6 +39,7 @@ async def factorial(n):
 async def stop_event_loop(loop, seconds):
     print(f"Stopping event loop in {seconds} seconds")
     await asyncio.sleep(seconds)
+    # loop.stop()
     print("Event loop stopped")
 
 
@@ -61,46 +62,43 @@ async def fetch_data(url):
 async def main():
     init_url = "https://picsum.photos/400/600"
     host = "www.youtube.com"
-    num_pings = 5
+    fetch_url_data = "http://example.com"
+    num_pings = 10
     num_photos = 10
 
     async with httpx.AsyncClient() as client:
-
         event_loop = asyncio.get_event_loop()
         shielded_stop = asyncio.shield(stop_event_loop(event_loop, 10))
 
         print("Start download photo")
         base_url_response = await client.get(init_url)
         base_url = base_url_response.url
+        list_urls = await download_photos(num_photos, base_url)
+        for url in list_urls[:5]:
+            print(f'Photo link: {url}')
 
         print("Start factorial")
-        factorial_tasks = [asyncio.create_task(factorial(n)) for n in range(20)]
+        factorial_tasks = [asyncio.create_task(factorial(n)) for n in range(30)]
 
         print("Start ping tasks")
         ping_tasks = [asyncio.create_task(ping(host)) for _ in range(num_pings)]
 
-        urls = await download_photos(num_photos, base_url)
-        for url in urls[:5]:
-            print(f'Photo link: {url}')
-
         results = await asyncio.gather(*ping_tasks, *factorial_tasks)
-        for result in results:
+        for result in results[:5]:
             print(result)
 
         print("Start fetch json data from database")
         data_task = [fetch_json() for _ in range(5)]
         data = await asyncio.gather(*data_task)
-
         if data:
-            print("Received data from the database:")
+            print("Received json data from the database:")
             print(data[:2])
         else:
-            print("Failed to fetch data from the database.")
+            print("Failed to fetch json data from the database.")
 
         print("Start fetch data from url")
-        url = 'http://example.com'
-        fetch_result = await fetch_data(url)
-        print(f"Response fetch data from {url}:\n {fetch_result}")
+        fetch_result = await fetch_data(fetch_url_data)
+        print(f"Response fetch data from {fetch_url_data}:\n {fetch_result}")
 
         await shielded_stop
 
